7adbc8598fb07a3d28fc2eb5cc445162afa48fc3
Kyuho Kim <valve.god.project@gmail.com>
2018-07-28 06:37:33 +0900

    Create 1_straight.ino

---
+++ 1_straight.ino
@@ -0,0 +1,25 @@
+void setup() {
+  for (int i=2;i<=13;i++)
+    pinMode(i, OUTPUT);
+}
+
+void on(int port) {
+  digitalWrite(port, HIGH);
+}
+
+void off(int port) {
+  digitalWrite(port, LOW);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  for (int i=2; i<=13; i++) {
+    on(i); 
+  } 
+  delay(1000); 
+  
+  for (int i=2; i<=13; i++) {
+    off(i);
+  }
+  delay(1000); 
+}
